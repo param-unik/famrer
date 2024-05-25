d28064152e00efd532676ce6ae6ca8d57d796989
Param <param.unik@gmail.com>
2024-05-25 13:57:38 +0530

    dvc pipeline

--- process_data.py
+++ process_data.py
@@ -1,16 +1,38 @@
 import pandas as pd
 
-df = pd.read_csv("data_raw.csv")
+df = pd.read_csv("data/rawdata_new_rev.csv")
 
 all_features = df.columns
 
 # Let's drop some features
-names = [feat for feat in all_features if "net_name" in feat] # excluded for privacy reasons
-useless = ["info_gew","info_resul","interviewtime","id","date"] # features that we expect are uninformative
-drop_list = names + useless 
+names = [
+    feat for feat in all_features if "net_name" in feat
+]  # excluded for privacy reasons
+useless = [
+    "info_gew",
+    "info_resul",
+    "interviewtime",
+    "id",
+    "date",
+]  # features that we expect are uninformative
+drop_list = names + useless
 
 # Remove the questionnaire about agricultural practices until I can better understand it
-practice_list = ["legum","conc","add","lact","breed","covman","comp","drag","cov","plow","solar","biog","ecodr"]
+practice_list = [
+    "legum",
+    "conc",
+    "add",
+    "lact",
+    "breed",
+    "covman",
+    "comp",
+    "drag",
+    "cov",
+    "plow",
+    "solar",
+    "biog",
+    "ecodr",
+]
 for feat in all_features:
     if any(x in feat for x in practice_list):
         drop_list.append(feat)
@@ -19,10 +41,9 @@ for feat in all_features:
 df = df.drop(columns=drop_list)
 
 # Convert non-numeric features to numeric
-non_numeric = list(df.select_dtypes(include=['O']).columns)
+non_numeric = list(df.select_dtypes(include=["O"]).columns)
 for col in non_numeric:
-    codes,uniques=pd.factorize(df[col])
+    codes, uniques = pd.factorize(df[col])
     df[col] = codes
 
 df.to_csv("data_processed.csv")
-
--- train.py
+++ train.py
@@ -1,4 +1,4 @@
-import pandas as pd 
+import pandas as pd
 import numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn import preprocessing
@@ -13,16 +13,16 @@ from sklearn.impute import SimpleImputer
 
 df = pd.read_csv("data_processed.csv")
 
-#### Get features ready to model! 
+#### Get features ready to model!
 y = df.pop("cons_general").to_numpy()
-y[y< 4] = 0
-y[y>= 4] = 1
+y[y < 4] = 0
+y[y >= 4] = 1
 
 X = df.to_numpy()
-X = preprocessing.scale(X) # Is standard
+X = preprocessing.scale(X)  # Is standard
 # Impute NaNs
 
-imp = SimpleImputer(missing_values=np.nan, strategy='mean')
+imp = SimpleImputer(missing_values=np.nan, strategy="mean")
 imp.fit(X)
 X = imp.transform(X)
 
@@ -31,23 +31,29 @@ X = imp.transform(X)
 clf = LogisticRegression()
 yhat = cross_val_predict(clf, X, y, cv=5)
 
-acc = np.mean(yhat==y)
+acc = np.mean(yhat == y)
 tn, fp, fn, tp = confusion_matrix(y, yhat).ravel()
-specificity = tn / (tn+fp)
+specificity = tn / (tn + fp)
 sensitivity = tp / (tp + fn)
 
 # Now print to file
-with open("metrics.json", 'w') as outfile:
-        json.dump({ "accuracy": acc, "specificity": specificity, "sensitivity":sensitivity}, outfile)
+with open("metrics.json", "w") as outfile:
+    json.dump(
+        {"accuracy": acc, "specificity": specificity, "sensitivity": sensitivity},
+        outfile,
+    )
 
 # Let's visualize within several slices of the dataset
 score = yhat == y
 score_int = [int(s) for s in score]
-df['pred_accuracy'] = score_int
+df["pred_accuracy"] = score_int
 
 # Bar plot by region
 
 sns.set_color_codes("dark")
-ax = sns.barplot(x="region", y="pred_accuracy", data=df, palette = "Greens_d")
-ax.set(xlabel="Region", ylabel = "Model accuracy")
-plt.savefig("by_region.png",dpi=80)
+ax = sns.barplot(
+    x="region", y="pred_accuracy", data=df, hue="region", palette="Greens_d"
+)
+ax.set(xlabel="Region", ylabel="Model accuracy")
+ax.legend_.remove()
+plt.savefig("by_region.png", dpi=80)
